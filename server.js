2fb48098f821354207cafb25c978a6a2e82121ce
Blake Schumann <blakeschumann15@gmail.com>
2021-08-23 14:35:36 +0930

    Setting up Express app to handle data parsing

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const path = require('path');
 
 let app = express();
 let PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+let notes = require("./db/db.json")
+
