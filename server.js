c59e5ec3a3cc634df33354ff13c10dc3beca4e4a
Blake Schumann <blakeschumann15@gmail.com>
2021-08-23 15:12:22 +0930

    adding create a note route

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.static("public"));
 
 let notes = require("./db/db.json")
 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
 app.get("./api/notes"), function (req, res) {
     fs.readFile("db/db.json", "utf8", function (err, data) {
         if (err) {
@@ -21,3 +25,27 @@ app.get("./api/notes"), function (req, res) {
     });
 };
 
+app.listen(PORT,function () {
+    console.log("app listening on PORT" + PORT);
+});
+
+app.post("/api/notes", function (req, res) {
+    let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    let id = randLetter + Date.now();
+    let newNote = {
+        id: id,
+        title: req.body.title,
+        text: req.body.text,
+    };
+    console.log(typeof notes);
+    notes.push(newNote);
+    const stringifyNote = JSON.stringify(notes);
+    res.json(notes);
+    fs.writeFile("db/db.json", stringifyNote, (err) => {
+        if (err)
+        console.log(err);
+        else {
+            console.log("note to successfully saved to db.json");
+        }
+    });
+});
