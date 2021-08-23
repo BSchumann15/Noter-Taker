64f18db90cbb25becf5af473a48fe99d45c2d279
Blake Schumann <blakeschumann15@gmail.com>
2021-08-23 15:31:06 +0930

    editing bracket and colons

--- server.js
+++ server.js
@@ -1,76 +1,76 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 let app = express();
-let PORT = process.env.PORT || 3001;
+let PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-let notes = require("./db/db.json")
+let notes = require("./db/db.json");
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("./api/notes"), function (req, res) {
-    fs.readFile("db/db.json", "utf8", function (err, data) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.json(notes);
-    });
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(notes);
+  });
 });
 
-app.listen(PORT,function () {
-    console.log("app listening on PORT" + PORT);
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
 
 app.post("/api/notes", function (req, res) {
-    let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    let id = randLetter + Date.now();
-    let newNote = {
-        id: id,
-        title: req.body.title,
-        text: req.body.text,
-    };
-    console.log(typeof notes);
-    notes.push(newNote);
-    const stringifyNote = JSON.stringify(notes);
-    res.json(notes);
-    fs.writeFile("db/db.json", stringifyNote, (err) => {
-        if (err)
-        console.log(err);
-        else {
-            console.log("note to successfully saved to db.json");
-        }
-    });
+  let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  let id = randLetter + Date.now();
+  let newNote = {
+    id: id,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  console.log(typeof notes);
+  notes.push(newNote);
+  const stringifyNote = JSON.stringify(notes);
+  res.json(notes);
+  fs.writeFile("db/db.json", stringifyNote, (err) => {
+    if (err) 
+    console.log(err);
+    else {
+    console.log("Note successfully saved to db.json");
+    }
+  });
 });
 
-app.delete("api/notes/:id", function (req, res) {
-    let noteID = req.params.id;
-    fs.readFile("db/db.json", "utf8", function (err, data) {
+app.delete("/api/notes/:id", function (req, res) {
+  let noteID = req.params.id;
+  fs.readFile("db/db.json", "utf8", function (err, data) {
     let updatedNotes = JSON.parse(data).filter((note) => {
-        console.log("note.id", note.id);
-        console.log("noteID", noteID);
-        return note.id !=== noteID;
+      console.log("note.id", note.id);
+      console.log("noteID", noteID);
+      return note.id !== noteID;
     });
     notes = updatedNotes;
     const stringifyNote = JSON.stringify(updatedNotes);
     fs.writeFile("db/db.json", stringifyNote, (err) => {
-        if (err)
-        console.log(err);
-        else {
-            console.log("note successfully deleted from db.json");
-        }
+      if (err) 
+      console.log(err);
+      else {
+      console.log("Note successfully deleted from db.json");
+      }
     });
     res.json(stringifyNote);
-});
+  });
 });
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
